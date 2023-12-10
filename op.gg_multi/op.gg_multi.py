00bc1b20f1d17416c6e4278101ae303a0ce072ec
kenkim365 <kimkenneth114@gmail.com>
2023-12-10 17:41:55 -0500

    removed unused code

--- op.gg_multi/op.gg_multi.py
+++ op.gg_multi/op.gg_multi.py
@@ -3,6 +3,3 @@ import pyperclip
 clipboard_content = pyperclip.paste()
 
 result = 'https://www.op.gg/multisearch/na?summoners=' + '%2C'.join(line.strip().replace(' ', '+').replace('#', '%23') for line in clipboard_content.split('\n'))
-
-
-print("Modified content copied to clipboard:", result)
