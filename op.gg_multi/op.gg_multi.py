e33f3610e29f1404d0c07b4d1a486a60e900f92e
kenkim365 <kimkenneth114@gmail.com>
2023-12-10 17:56:38 -0500

    added comment

--- op.gg_multi/op.gg_multi.py
+++ op.gg_multi/op.gg_multi.py
@@ -1,5 +1,7 @@
 import pyperclip
 
+# breaks if more than 10 usernames are copied
+
 clipboard_content = pyperclip.paste()
 
 result = 'https://www.op.gg/multisearch/na?summoners=' + '%2C'.join(line.strip().replace(' ', '+').replace('#', '%23') for line in clipboard_content.split('\n'))
