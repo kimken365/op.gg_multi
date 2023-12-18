a2db68c2b7ae52f4bc43a643902311e23c37ae63
kenkim365 <kimkenneth114@gmail.com>
2023-12-17 20:52:42 -0500

    undo edits

--- op.gg_multi/op.gg_multi.py
+++ op.gg_multi/op.gg_multi.py
@@ -2,6 +2,6 @@ import pyperclip
 
 # breaks if more than 10 usernames are copied
 
-    clipboard_content = pyperclip.paste()
+clipboard_content = pyperclip.paste()
 
-    result = 'https://www.op.gg/multisearch/na?summoners=' + '%2C'.join(line.strip().replace(' ', '+').replace('#', '%23') for line in clipboard_content.split('\n'))
+result = 'https://www.op.gg/multisearch/na?summoners=' + '%2C'.join(line.strip().replace(' ', '+').replace('#', '%23') for line in clipboard_content.split('\n'))
