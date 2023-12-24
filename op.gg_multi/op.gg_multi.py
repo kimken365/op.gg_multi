465808fe7c11bac97b5efe3785909dfbc032f6c1
kenkim365 <kimkenneth114@gmail.com>
2023-12-24 02:36:44 -0500

    store url into clipboard

--- op.gg_multi/op.gg_multi.py
+++ op.gg_multi/op.gg_multi.py
@@ -5,3 +5,5 @@ import pyperclip
 clipboard_content = pyperclip.paste()
 
 result = 'https://www.op.gg/multisearch/na?summoners=' + '%2C'.join(line.strip().replace(' ', '+').replace('#', '%23') for line in clipboard_content.split('\n'))
+
+pyperclip.paste(result)
